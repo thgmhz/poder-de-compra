21d01da1e25e6bad00125045d6efd5d4acfa902a
Thiago Munhoz <thiago.munhoz@hhsurge.com>
2019-08-08 17:00:01 +0100

    Updates

---
+++ precache-manifest.003ed0212a3b7120c543cd8d317a24ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a396efb5efd18a92f174dea2d4d486",
+    "url": "/howme/index.html"
+  },
+  {
+    "revision": "39d7b53525f6344ed7e8",
+    "url": "/howme/static/css/main.88a00211.chunk.css"
+  },
+  {
+    "revision": "d247f43a7bd7717c1982",
+    "url": "/howme/static/js/2.9bf5f935.chunk.js"
+  },
+  {
+    "revision": "39d7b53525f6344ed7e8",
+    "url": "/howme/static/js/main.90c00221.chunk.js"
+  },
+  {
+    "revision": "883028c57646dd76f64f",
+    "url": "/howme/static/js/runtime~main.15f1bac5.js"
+  },
+  {
+    "revision": "a102e5631626f71ddbc427003063dc0a",
+    "url": "/howme/static/media/brazil.a102e563.svg"
+  }
+]);
